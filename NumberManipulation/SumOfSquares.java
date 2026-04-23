da4ad7f3af524f3a5109c4594f23e68adf2a3123
veeresh <pasareveeresh0908@gmail.com>
2026-04-23 08:42:00 +0530

    added pattern problems

---
+++ NumberManipulation/SumOfSquares.java
@@ -0,0 +1,12 @@
+package NumberManipulation;
+
+public class SumOfSquares {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum =0;
+        for(int i =1;i<=n;i++){
+        sum += i * i;
+        }
+        System.out.println(sum);
+    }
+}
