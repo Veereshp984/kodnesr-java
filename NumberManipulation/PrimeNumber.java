31051ce1d15b705a5a476d4d6f0523bc7d0d1b44
veeresh <pasareveeresh0908@gmail.com>
2026-04-23 09:36:18 +0530

    added pattern problems

--- NumberManipulation/PrimeNumber.java
+++ NumberManipulation/PrimeNumber.java
@@ -1,4 +1,18 @@
 package NumberManipulation;
 
 public class PrimeNumber {
+    public static void main(String[] args) {
+        int n = 17;
+        int count =0;
+        for(int i =1;i<=n;i++){
+            if(n % i ==0){
+                count++;
+            }
+        }
+        if(count == 2){
+            System.out.println("the given number is prime number");
+        }else{
+            System.out.println("the given number is not a prime number");
+        }
+    }
 }
