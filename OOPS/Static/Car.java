8e307b042c9fdea09c15bf61823eeaf99a0e0f48
veeresh <pasareveeresh0908@gmail.com>
2026-05-04 09:04:24 +0530

    started string problems

---
+++ OOPS/Static/Car.java
@@ -0,0 +1,10 @@
+package OOPS.Static;
+
+public class Car {
+    static void convertKmToMiles(){
+        System.out.println("converting to km to miles");
+    }
+    void calculateMilage(){
+        System.out.println("calculating milage");
+    }
+}
---
+++ OOPS/Static/CarApplication.java
@@ -0,0 +1,9 @@
+package OOPS.Static;
+
+public class CarApplication {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.convertKmToMiles();
+        c.calculateMilage();
+    }
+}
