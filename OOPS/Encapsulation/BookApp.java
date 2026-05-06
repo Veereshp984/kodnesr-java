32d717d408fffab20bcc6932a675dbb7cb1079f3
veeresh <pasareveeresh0908@gmail.com>
2026-05-06 09:47:23 +0530

    started string problems

--- OOPS/Encapsulation/BookApp.java
+++ OOPS/Encapsulation/BookApp.java
@@ -4,6 +4,9 @@ public class BookApp {
     public static void main(String[] args) {
         // process of providing the controlled access to the
         // private data members of the class using the member functions.
+        //or
+//        process of putting the data(variables) and methods(functions) together inside one class and hiding the
+//        internal details from other class
         Book b = new Book();
         b.getData(100);
         b.setData();
