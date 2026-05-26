9f60e83cdb8f1efd596c1172a3bc1aa72531613c
veeresh <pasareveeresh0908@gmail.com>
2026-05-26 14:37:42 +0530

    added the composition and aggregation concept in OOPS

--- OOPS/Aggregation/Practice3.java
+++ OOPS/Aggregation/Practice3.java
@@ -1,4 +1,21 @@
 package OOPS.Aggregation;
-
+class Room {
+    void showRoom(){
+        System.out.println("Room is Available");
+    }
+}
+class House {
+    Room room;
+    House(){
+        room = new Room();
+    }
+    void showHouse(){
+        room.showRoom();
+    }
+}
 public class Practice3 {
+    public static void main(String[] args) {
+        House house = new House();
+        house.showHouse();
+    }
 }
